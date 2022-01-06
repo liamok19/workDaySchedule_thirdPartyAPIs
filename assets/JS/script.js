fd095073dac0d2f84fcf6f912b17f3ab4500b7ab
Liam Ok <liamokane19@gmail.com>
2022-01-06 22:20:45 +1100

    timeblock descriptions are now updating according to the current hour and changing colour accordingly if the hour is past, present or future

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,2 +1,33 @@
+//current day of the week, and date and time of the year
+
 var today = moment();
-$("#currentDay").text(today.format("dddd MMM Do, YYYY"));
+    $("#currentDay").text(today.format("dddd MMM Do, YYYY k:mm:ss"));
+
+//change colo(u)r of description cell based on time of day
+var hours = document.getElementsByClassName("hour");
+    console.log(hours);
+
+var currentHour = 10 //parseInt(moment().format('H')); 
+    console.log(currentHour);
+
+Array.from(hours).forEach(hour => {
+    console.log(hour.dataset);
+    if (currentHour == hour.dataset.hour) {
+        $(hour).next().addClass("present"); //Current Hour
+    } else if ((currentHour < hour.dataset.hour)) {
+        $(hour).next().addClass("future"); //Future Hour/s
+    } else { 
+        $(hour).next().addClass("past"); //Past Hour 
+    }
+});
+
+// function setColor(element, color) {
+//     element.style.backgroundColor = color;
+// }
+
+// $("td#9").css("color", "green");
+
+
+//data attribute 
+
+//parseInt - 
